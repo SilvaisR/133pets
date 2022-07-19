9a7bddd0963e9983be7dc8fd8cbcd02f54fc6306
Silvanis <silvanis.silva@outlook.com>
2022-07-19 14:46:23 -0300

    Isso

--- tests/api/pet.py
+++ tests/api/pet.py
@@ -1,8 +1,26 @@
+import csv
+
 import pytest  # motor  /  engine
 import requests  # bilioteca para comunicar com APIs
 
 base_url = 'https://petstore.swagger.io/v2'  # endereço da API
 headers = {'Content-Type': 'application/json'}  # os dados serão    no formato json
+
+
+def ler_dados_scv():
+    dados_csv = []  # Criamos uma lista vazia.
+    nome_arquivo = r'C:\Users\silva\PycharmProjects\pythonProject133pets\vendors\csv\pets_positivo.csv'
+    try:
+        with open(nome_arquivo, newline='') as csvfile:
+            campos = csv.reader(csvfile, delimiter=',')
+            next(campos)
+            for linha in campos:
+                dados_csv.append(linha)
+            return dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {nome_arquivo}')
+    except Exception as fail:
+        print(f'Falha não prevista: {fail}')
 
 
 def testar_incluir_pet():
@@ -92,3 +110,53 @@ def testar_excluir_pet():
     assert response_body['code'] == code_esperado
     assert response_body['type'] == type_esperado
     assert response_body['message'] == message_esperada
+
+
+@pytest.mark.parametrize('pet_id,category_id,category_name,name,tags_id,tags_name,status,status_code', ler_dados_scv())
+def testar_incluir_pet_json_dinamico(pet_id, category_id, category_name, name, tags_id, tags_name, status, status_code):
+    # configura - Os dados e o resultado esperado.
+    # 1.1 - Dados de entrada
+    # Utilizará o arquivo pets_positivo.csv
+
+    # 1.2 - Resultado esperado
+    # Utilizará o arquivo pets_positivo.csv
+
+    # 1.3 - Extra - montar o json dinamicamento a partir do csv
+    corpo_json = '{'
+    corpo_json += f'  "id": {pet_id},'
+    corpo_json += '  "category": {'
+    corpo_json += f'    "id": {category_id},'
+    corpo_json += f'    "name": {category_name}'
+    corpo_json += '},'
+    corpo_json += f'"name": {name},'
+    corpo_json += '"photoUrls": ['
+    corpo_json += '"string"'
+    corpo_json += '],'
+    corpo_json += '"tags": ['
+    corpo_json += '{'
+    corpo_json += f'    "id": {tags_id},'
+    corpo_json += f'    "name": {tags_name}'
+    corpo_json += '}'
+    corpo_json += '],'
+    corpo_json += f'"status": {status}'
+    corpo_json += '}'
+
+    print(corpo_json)
+    # executa
+    resultado_obtido = requests.post(
+        url=base_url + '/pet',
+        data=corpo_json,
+        headers=headers
+    )
+
+    # valida
+    assert resultado_obtido.status_code == status_code
+    corpo_da_resposta = resultado_obtido.json()
+    print(corpo_da_resposta)
+    assert corpo_da_resposta['id'] == pet_id
+    assert corpo_da_resposta['category'][0]['id'] == category_id
+    assert corpo_da_resposta['category'][0]['name'] == category_name
+    assert corpo_da_resposta['name'] == name
+    assert corpo_da_resposta['tags'][0]['id'] == tags_id
+    assert corpo_da_resposta['tags'][0]['name'] == tags_name
+    assert corpo_da_resposta['status'] == status
